ec201588764dcfb0511a6a4341408d8cb67db56e
Ricardo Menotti <ricardomenotti31@gmail.com>
2023-02-18 11:52:52 -0500

    link

--- src/components/navbars/RegularNavBar/index.tsx
+++ src/components/navbars/RegularNavBar/index.tsx
@@ -104,7 +104,7 @@ const RegularNavBar = () => {
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
-            <a className="block text-teal-600" href="/">
+            <Link className="block text-teal-600" href="/">
               <span className="sr-only">Home</span>
               <svg
                 className="h-8"
@@ -117,7 +117,7 @@ const RegularNavBar = () => {
                   fill="currentColor"
                 />
               </svg>
-            </a>
+            </Link>
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
@@ -132,12 +132,12 @@ const RegularNavBar = () => {
 
             <div className="flex items-center gap-4">
               <div className="hidden sm:flex sm:gap-4">
-                <a
+                <Link
                   className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
                   href="/"
                 >
                   Contacto
-                </a>
+                </Link>
               </div>
 
               <div className="md:hidden relative">
@@ -200,12 +200,12 @@ const RegularNavBar = () => {
 
                       <div className="my-4 border-t" />
 
-                      <a
+                      <Link
                         className="rounded-md bg-teal-600 px-5 py-2.5 font-medium text-white shadow w-full text-center"
                         href="/"
                       >
                         Contacto
-                      </a>
+                      </Link>
                     </div>
                   </div>
                 )}
