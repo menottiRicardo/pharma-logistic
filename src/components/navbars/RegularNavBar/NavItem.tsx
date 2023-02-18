f742424841f393dcc0b86a53d7a2fe1cec098472
Ricardo Menotti <ricardomenotti31@gmail.com>
2023-02-18 11:45:54 -0500

    small menu

--- src/components/navbars/RegularNavBar/NavItem.tsx
+++ src/components/navbars/RegularNavBar/NavItem.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import React from "react";
 
-const NavItem = ({ navName, href }: { navName: string; href: string }) => {
+const NavItem = ({
+  navName,
+  href,
+  color = "text-gray-500",
+}: {
+  navName: string;
+  href: string;
+  color?: string;
+}) => {
   return (
     <Link
       href={href}
-      className="flex items-center gap-x-1 p-2 cursor-pointer text-gray-500 hover:text-gray-900"
+      className={`flex items-center gap-x-1 p-2 cursor-pointer ${color} hover:text-gray-900`}
     >
-      <span className="text-base font-medium">{navName}</span>
+      <li className="text-base font-medium">{navName}</li>
     </Link>
   );
 };
